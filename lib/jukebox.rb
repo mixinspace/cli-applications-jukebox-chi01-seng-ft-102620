f22c9f8485cda45f0fcdc020df6e76eb6abb682d
Islam Madkaur <islammadkaur@gmail.com>
2020-10-25 00:59:23 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,47 +1,51 @@
-# Add your code here
-def run(songs)
-  help
-  while true do
-    puts "Please enter a command:"
-    response = gets.chomp
-    case response
-    when "exit"
-      exit_jukebox
-      break
-    when "play"
-      play(songs)
-    when "help"
-      help
-    when "list"
-      list(songs)
-    else
-      puts "Invalid entry"
+def run(songs_array)
+  puts "Please enter a command:"
+  user_input = gets.strip
+
+    case user_input
+  when "help"
+    help
+  when "list"
+    list(songs_array)
+  when "play"
+    play(songs_array)
+  when "exit"
+    return exit_jukebox
+  end
+end
+
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+def play(songs_array)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+
+  songs_array.each_with_index do |element, index|
+    index += 1
+    if input.to_i == index || input == element
+      return puts "Playing #{element}"
     end
   end
+  puts "Invalid input, please try again"
 end
-def play(songs)
-  puts "Please enter a song name or number:"
-  response = gets.chomp
-  if response.to_i >= 1 && response.to_i <= songs.length
-    puts "Playing #{songs[response.to_i-1]}"
-  elsif songs.include?(response)
-    puts "Playing #{songs.find{|song| song == response}}"
-  else
-    puts "Invalid input, please try again"
+
+
+def list(songs_array)
+  songs_array.each_with_index do |element, index|
+    index += 1
+  puts "#{index}. #{element}"
   end
 end
+
+
 def exit_jukebox
   puts "Goodbye"
-end
-def help
-puts  "I accept the following commands:"
-puts  "- help : displays this help message"
-puts  "- list : displays a list of songs you can play"
-puts  "- play : lets you choose a song to play"
-puts  "- exit : exits this program"
-end
-def list(songs)
-songs.each_with_index {|song, index|
-  puts "#{index+1}. #{song}"
-}
 end
